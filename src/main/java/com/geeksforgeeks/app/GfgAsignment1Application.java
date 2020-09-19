2f9cbed1b2938a7fa03a1cdf97e37635bd27dc2c
Dharmendra Kumar Awasthi <awadha01@CSGICORP.COM>
2020-09-19 11:08:58 +0530

    Initialized project

---
+++ src/main/java/com/geeksforgeeks/app/GfgAsignment1Application.java
@@ -0,0 +1,13 @@
+package com.geeksforgeeks.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfgAsignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfgAsignment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/geeksforgeeks/app/GfgAsignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geeksforgeeks.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfgAsignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
